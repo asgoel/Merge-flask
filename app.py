038182c91e1866a2a248d4b608dc7f4142999599
Ashu Goel <soxfan981@gmail.com>
2013-01-18 21:11:51 -0500

    sqlalchemy boilerplate

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 from flask import Flask, request, redirect, url_for, flash, render_template
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask_heroku import Heroku
 app = Flask(__name__)
+heroku = Heroku(app)
+db = SQLAlchemy(app)
+
 
 @app.route('/')
 def index():
