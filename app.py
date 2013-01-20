02da69f49771f996a7e4bcbc2aa746782c639131
Max Scheiber <keasbeynumb@gmail.com>
2013-01-20 04:29:42 -0500

    test at changing API

--- app.py
+++ app.py
@@ -408,8 +408,8 @@ def join_event():
 # grabs all events currently going on from the user's university in a given category
 @app.route('/event', methods=['GET'])
 def get_events():
-  data = request.json
-  apikey = data["apikey"]
+  data = request.args # data = request.json
+  apikey = data['apikey']
   user = Person.query.filter_by(apikey=apikey).first()
   if user is None:
     data = {
