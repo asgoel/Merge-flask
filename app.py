8fe55ae2201a1b7254f0574d5e11be6438fc3733
Max Scheiber <keasbeynumb@gmail.com>
2013-01-20 04:34:14 -0500

    fuck

--- app.py
+++ app.py
@@ -408,6 +408,7 @@ def join_event():
 # grabs all events currently going on from the user's university in a given category
 @app.route('/event/<apikey>/<category>', methods=['GET'])
 def get_events():
+  print "hits"
   #data = request.args # data = request.json
   #apikey = data['apikey']
   user = Person.query.filter_by(apikey=apikey).first()
